5e05f20b8739c3dbd4ba0eaa1599e27c8278cbe0
Tw3nty <silas.reiling@hotmail.de>
2022-11-19 18:13:29 +0100

    Added new route to get single stock history data

--- djangoBackend/api/views.py
+++ djangoBackend/api/views.py
@@ -1,8 +1,27 @@
 from rest_framework.response import Response
+from django.http import Http404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from base.models import Stock_price_table
 from base.models import Stock_table
 from .serializers import Stock_price_table_serializer, Stock_table_serializer
+
+from itertools import chain
+
+@api_view(["GET"])
+def stock_price_data(request, stock_symbol):
+    all_stock_symbols_tuple = list(Stock_table.objects.values_list("ticker_symbol")) #Output is list of nested tuples
+    all_stock_symbols = list(chain.from_iterable(all_stock_symbols_tuple)) # Flatten the tuple list to list of strings
+    
+    if stock_symbol not in all_stock_symbols:
+        return Response("Error. This stock symbol is not tracked or recognised. Check spelling", status=status.HTTP_404_NOT_FOUND)
+    
+    # Get all rows where ticker_symbol equals requested stock_symbol
+    requested_stock_data = Stock_price_table.objects.filter(ticker_symbol = stock_symbol).values()
+    
+    serializer = Stock_price_table_serializer(requested_stock_data, many=True)
+    
+    return Response(serializer.data)
 
 @api_view(["GET"])
 def get_all_stock_prices(request):
@@ -16,7 +35,6 @@ def addStock(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
-
 
 @api_view(["GET"])
 def get_all_stock_symbols(request):

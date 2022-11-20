8431495ae05ffeebf79894d48b08c8bc93cd1b51
Tw3nty <silas.reiling@hotmail.de>
2022-11-20 11:09:52 +0100

    Testpage now shows all charts

--- djangoBackend/api/urls.py
+++ djangoBackend/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("stop_gathering/", views.stop_gathering),
     path("all_stock_prices/", views.get_all_stock_prices),
     path("all_stock_symbols/", views.get_all_stock_symbols),
     path("stock_price_data/<stock_symbol>", views.stock_price_data),
--- stock-frontend/src/app/test-page/test-page.component.ts
+++ stock-frontend/src/app/test-page/test-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import { Observable } from 'rxjs';
 import { Chart } from "chart.js/auto"
 
 @Component({
@@ -10,51 +9,52 @@ import { Chart } from "chart.js/auto"
 })
 export class TestPageComponent implements OnInit {
   chart;
-  ctx;
-
   xData;
   yData;
-  dataExample = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
-  stockData;
+  stock_symbols = [];
+  stock_data;
   dataReceived = false;
+
   constructor(private http:HttpClient) {
-    this.http.get("http://localhost:8000/stock_price_data/aapl")
+    // Get all symbols
+    this.http.get("http://localhost:8000/all_stock_symbols")
     .subscribe(data => {
+      for(let i = 0; i < Object.entries(data).length; i++){
+        this.stock_symbols.push(Object.entries(data)[i][1]["ticker_symbol"]);
+      }
+      
+      console.log(this.stock_symbols);
+    
+    this.stock_symbols.forEach(function(symbol){
+      
+      http.get("http://localhost:8000/stock_price_data/" + symbol)
+      .subscribe(data => {
+        // Convert it to readable format
+        let stock_data = Object.entries(data).map(entry => entry[1]);
+        
+        console.log(stock_data);
+        
+        // Map time to x and price to y axis data
+        let xData = stock_data.map(row => row["date_time"])
+        let yData = stock_data.map(row => row["price"])
+        
+        this.renderChart(xData, yData, symbol)
+    })}.bind(this))
+  this.dataReceived = true});
 
-      // Convert it to readable format
-      this.stockData = Object.entries(data).map(entry => entry[1]);
-      
-      console.log(this.stockData);
-      
-      // Map time to x and price to y axis data
-      this.xData = this.stockData.map(row => row["date_time"])
-      this.yData = this.stockData.map(row => row["price"])
-      
-      this.renderChart()
-      this.ctx = document.getElementById('canvas');
-    })
   }
-
 
   ngOnInit(): void {
   }
 
-  public renderChart(){
-    this.chart = new Chart("canvas", {
+  public renderChart(xData, yData, symbol){
+    this.chart = new Chart("canvas" + symbol, {
       type: "line",
       data : {
-        labels: this.xData,
+        labels: xData,
         datasets: [{
-          label: "Prices",
-          data: this.yData,
+          label: symbol,
+          data: yData,
           backgroundColor: "red",
           borderColor: "red",
         }
@@ -62,9 +62,4 @@ export class TestPageComponent implements OnInit {
       }
     })
   }
-
-  public getStockData(){
-    return this.stockData;
-  }
-
 }

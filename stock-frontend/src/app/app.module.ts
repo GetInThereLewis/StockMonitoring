3cba98a5adaa64e64134330f839d830f2210cdf4
GetInThereLewis <85756773+GetInThereLewis@users.noreply.github.com>
2022-11-20 16:21:37 +0100

    tst

--- stock-frontend/src/app/app.module.ts
+++ stock-frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FullListingsComponent } from './full-listings/full-listings.component';
 import { AddListingRestrictedComponent } from './add-listing-restricted/add-listing-restricted.component';
@@ -13,17 +12,19 @@ import {HttpClientModule} from '@angular/common/http';
 import { ContentBoxComponent } from './content-box/content-box.component';
 import { FooterComponent } from './footer/footer.component';
 import { TestPageComponent } from './test-page/test-page.component';
+import { BootstrapnavComponent } from './bootstrapnav/bootstrapnav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
+    
     LandingPageComponent,
     FullListingsComponent,
     AddListingRestrictedComponent,
     ContentBoxComponent,
     FooterComponent,
     TestPageComponent,
+    BootstrapnavComponent,
     
   ],
   imports: [
